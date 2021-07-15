4c7cf91cffb4a9eced68c14f4be32019ce86446c
Wilton Ramos <wrs@cin.ufpe.br>
2021-07-15 10:28:46 -0300

    creates the recipe codable

--- paprika/Models/Recipe.swift
+++ paprika/Models/Recipe.swift
@@ -9,13 +9,48 @@ import Foundation
 struct Recipe: Codable {
     let name: String
     let id: Int
-    let image, lvl: String
+    let image: String
+    let lvl: LvlEnum
     let favorited: Bool
-    let category: [String]
-    let ingredients: [[String]]
-    let portion: String
+    let category: [CategoryEnum]
+    let ingredients: [[Ingredient]]
+    let portion: Int
     let tools: [String]
     let timer: [[Int]]
     let totalTime: Int
-    let stepByStep: [[String]]
+    let stepByStep: [[StepByStep]]
 }
+
+// MARK: - Ingredient
+struct Ingredient: Codable {
+    let name: String
+    let amount: Float
+    let measure: String
+}
+
+// MARK: - StepByStep
+struct StepByStep: Codable {
+    let title: String
+    let description: String
+}
+
+// MARK: - LvlEnum
+enum LvlEnum: String, Codable {
+    case facil = "Fácil"
+    case medio = "Intermediário"
+    case dificil = "Difícil"
+}
+
+// MARK: - CategoryEnum
+enum CategoryEnum: String, Codable {
+    case sobremesa = "Sobremesa"
+    case cafeDaManha = "Café da manhã"
+    case almoco = "Almoço"
+    case janta = "Janta"
+    case vegano = "Vegano"
+    case vegetariano = "Vegetariano"
+}
+
+
+
+
