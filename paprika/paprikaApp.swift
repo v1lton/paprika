cf325dc2767bf548851fb4b65c6f2364f4cee5c8
Wilton Ramos <wrs@cin.ufpe.br>
2021-07-15 08:36:41 -0300

    adds paprika.xcodeproj

---
+++ paprika/paprikaApp.swift
@@ -0,0 +1,20 @@
+//
+//  paprikaApp.swift
+//  paprika
+//
+//  Created by Wilton Ramos on 15/07/21.
+//
+
+import SwiftUI
+
+@main
+struct paprikaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
