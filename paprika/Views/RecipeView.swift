e94ec66776d67e57e29efcf3ac9d5ee28efa2feb
Wilton Ramos <wrs@cin.ufpe.br>
2021-07-21 09:04:08 -0300

    Creates RecipeView

---
+++ paprika/Views/RecipeView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeView.swift
+//  paprika
+//
+//  Created by Wilton Ramos on 21/07/21.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeView()
+    }
+}
