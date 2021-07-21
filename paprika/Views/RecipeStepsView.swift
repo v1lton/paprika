b346adc671acf01dcde3bd64d245f4a6df825ff2
Wilton Ramos <wrs@cin.ufpe.br>
2021-07-21 09:04:53 -0300

    Adds todo

--- paprika/Views/RecipeStepsView.swift
+++ paprika/Views/RecipeStepsView.swift
@@ -22,7 +22,7 @@ struct RecipeStepsView: View {
             
             TabView {
                 ForEach(viewModel.recipeSteps, id:\.self) { step in
-                    HStack {
+                    AdaptiveStack {
                         
                         //Title box
                         VStack {
@@ -67,3 +67,31 @@ struct RecipeStepsView_Previews: PreviewProvider {
         
     }
 }
+
+struct AdaptiveStack<Content: View>: View {
+    @Environment(\.horizontalSizeClass) var sizeClass
+    let horizontalAlignment: HorizontalAlignment
+    let verticalAlignment: VerticalAlignment
+    let spacing: CGFloat?
+    let content: () -> Content
+
+    init(horizontalAlignment: HorizontalAlignment = .center, verticalAlignment: VerticalAlignment = .center, spacing: CGFloat? = nil, @ViewBuilder content: @escaping () -> Content) {
+        self.horizontalAlignment = horizontalAlignment
+        self.verticalAlignment = verticalAlignment
+        self.spacing = spacing
+        self.content = content
+    }
+
+    var body: some View {
+        Group {
+            if sizeClass == .compact {
+                VStack(alignment: horizontalAlignment, spacing: spacing, content: content)
+            } else {
+                HStack(alignment: verticalAlignment, spacing: spacing, content: content)
+            }
+        }
+    }
+}
+
+//TODO: Fazer um adapative stack baseado no geometryReader
+//FIXME: OI SWIFTUI
