66baf72a9111c723334fb67e3413420b8daee84e
Wilton Ramos <wrs@cin.ufpe.br>
2021-07-26 09:27:07 -0300

    Inserts buttons in the final page

--- paprika/Views/RecipeStepsView.swift
+++ paprika/Views/RecipeStepsView.swift
@@ -21,11 +21,33 @@ struct FinalStepView: View {
                 }.frame(width: geometry.size.width * 0.45, height: geometry.size.height, alignment: .topLeading)
                 
                 VStack {
-                    Text("Title")
+                    Text("Bom apetite!")
                         .font(.custom("Albra Semi", size: 56))
                         .foregroundColor(Color("BrandSecondary400"))
+                        .frame(width: geometry.size.width * 0.50, alignment: .leading)
                     
                     Text("Lembrou de alguém que também iria gostar dessa receita? Compartilhe! \nVocê também pode salvá-la nos seus Favoritos para ver sempre que quiser.")
+                        .font(.custom("SF-Pro-Display-Medium", size: 32))
+                        .lineSpacing(10)
+                    
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Text("Compartilhar essa receita")
+                            .foregroundColor(Color("BrandPrimary400"))
+                            .font(.custom("SF-Pro-Display-Bold", size: 24))
+                                                        
+                    })
+                    .frame(width: geometry.size.width * 0.50, height: 56, alignment: .center)
+                    .foregroundColor(Color("PrimitiveWhite"))
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Text("Salvar nos favoritos")
+                            .foregroundColor(Color("BrandPrimary400"))
+                            .font(.custom("SF-Pro-Display-Bold", size: 24))
+                    })
+                    .frame(width: geometry.size.width * 0.50, height: 56, alignment: .center)
+                    .foregroundColor(Color("PrimitiveWhite"))
+                    
                 }.frame(width: geometry.size.width * 0.50, height: geometry.size.height, alignment: .topTrailing)
                 
             }
@@ -64,9 +86,8 @@ struct RecipeStepsView: View {
                         //Recipe box
                         VStack {
                             Text(step.stepByStepDescription)
-                                .fontWeight(.regular)
                                 .lineSpacing(10)
-                                .font(.system(size: 32))
+                                .font(.custom("SF-Pro-Display-Medium", size: 32))
                         }.frame(width: geometry.size.width * 0.65, height: geometry.size.height, alignment: .topLeading)
                         
                     }.frame(height: geometry.size.height, alignment: .topLeading)
