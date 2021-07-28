cce7378911b47160e6516822e13b5ee83ac1423a
Íris Soares <iss3@cin.ufpe.br>
2021-07-28 12:10:46 -0300

    remove favorited button until i can fix it

--- paprika/Components/CardsScroll.swift
+++ paprika/Components/CardsScroll.swift
@@ -20,8 +20,7 @@ struct CardsScroll: View {
                         label: {
                             Card(photo: Binding.constant(recipe.image),
                                  title: Binding.constant(recipe.name),
-                                 tag: Binding.constant(recipe.lvl),
-                                 favorited: Binding.constant(recipe.favorited))
+                                 tag: Binding.constant(recipe.lvl))
                         }).navigationTitle("Início")
                 }
             }.padding(16)
