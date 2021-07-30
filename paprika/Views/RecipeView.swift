c298b9870bcac1ab1eabfe5c0161d99265d1b8b4
Wilton Ramos <wrs@cin.ufpe.br>
2021-07-30 10:48:56 -0300

    Sets the left view

--- paprika/Views/RecipeView.swift
+++ paprika/Views/RecipeView.swift
@@ -17,6 +17,12 @@ struct RecipeView: View {
             HStack() {
                 LeftView(recipe: recipe)
                     .frame(width: geometry.size.width * 0.45)
+                    .padding(.leading, 20)
+                    .padding(.bottom, 20)
+                
+                Spacer()
+                
+                Divider()
                 
                 Spacer()
                 
@@ -47,61 +53,135 @@ struct LeftView: View {
                 VStack{
                     HStack{
                         Text(recipe.name)
+                            .font(.custom("Albra Semi", size: 44))
+                            .foregroundColor(Color.primitiveBlack)
+                            .minimumScaleFactor(0.5)
+                        
                         Spacer()
                     }
                     
                     
                     HStack{
                         Text("por")
-                        Text((recipe.author) != "" ? recipe.author : "Paprika")
+                            .font(.custom("SF Pro Display Regular", size: 16))
+                            .foregroundColor(Color.primitive600)
+                            .padding(.trailing, -2)
+                        
+                        if recipe.author != ""{
+                            Text(recipe.author)
+                                .font(.custom("SF Pro Display Regular", size: 16))
+                                .foregroundColor(Color.brandPrimary400)
+                                .underline()
+                                .onTapGesture {
+                                    if recipe.creditsURL != "" {
+                                        UIApplication.shared.open(URL(string: recipe.creditsURL)!, options: [:])
+                                    }
+                                }
+                        } else {
+                            Text("Paprika")
+                                .font(.custom("SF Pro Display Regular", size: 16))
+                                .foregroundColor(Color.brandPrimary400)
+                        }
+                        
+                        
+                        
                         Spacer()
                     }
                 }
                 
+                Spacer()
+                    .frame(height: 24)
+                
+                //MARK: -Info box
                 HStack {
+                    
                     VStack(alignment: .leading) {
                         Spacer()
                         HStack {
-                            Text("Nível de dificuldade:")
+                            Spacer()
+                                .frame(width: 16)
                             
-                            Text(recipe.lvl)
+                            Image(systemName: "flag")
+                                .font(.system(size: 20, weight: .semibold))
+                                .foregroundColor(Color.brandSecondary400)
+                            
+                            Text("Nível de dificuldade:")
+                                .font(.custom("SF Pro Display Semibold", size: 20))
+                                .foregroundColor(Color.brandSecondary400)
+                            
+                            Text((recipe.lvl == "Fácil") ? "Iniciante" : recipe.lvl)
+                                .font(Font.system(size: 16))
+                                .foregroundColor((recipe.lvl == "Fácil") ? .positiveFeedback800 : (recipe.lvl == "Intermediário") ? .alertFeedback700 : .brandPrimary600)
+                                .padding(.horizontal, 4)
+                                .padding(.vertical, 6)
+                                .background((recipe.lvl == "Fácil") ? Color.positiveFeedback50 : (recipe.lvl == "Intermediário") ? Color.alertFeedback50 : Color.alertFeedback50)
+                                .cornerRadius(8)
                         }
                         Spacer()
                         HStack{
+                            Spacer()
+                                .frame(width: 16)
+                            
+                            Image(systemName: "clock")
+                                .font(.system(size: 20, weight: .semibold))
+                                .foregroundColor(Color.brandSecondary400)
+                            
                             Text("Tempo de Preparo:")
+                                .font(.custom("SF Pro Display Semibold", size: 20))
+                                .foregroundColor(Color.brandSecondary400)
                             
                             Text((recipe.totalTime) >= 60 ? "~\(recipe.totalTime/60)h" : "~\(recipe.totalTime)min")
+                                .font(.custom("SF Pro Display Semibold", size: 20))
+                                .foregroundColor(Color.primitive700)
                         }
                         Spacer()
-                    }.frame(width: 377, height: 100)
+                    }
+                    .frame(width: geometry.size.width * 0.9, height: 100, alignment: .leading)
                     .background(Color.primitiveWhite)
                     .overlay(
                         RoundedRectangle(cornerRadius: 8)
-                                   .stroke(Color.brandSecondary400, lineWidth: 1)
+                            .stroke(Color.brandSecondary400, lineWidth: 1)
                     )
                     
                     Spacer()
-
                 }
                 
                 Spacer()
                 
+                //MARK: - Bottom buttons
                 HStack {
                     
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Começar o passo a passo")
-                    })
+                    NavigationLink(
+                        destination: RecipeStepsView(viewModel: RecipeStepsView.ViewModel(), recipeSteps: recipe.stepByStep, recipeImage: recipe.image),
+                        label: {
+                            HStack{
+                                Text("Começar o passo a passo")
+                                    .font(.custom("SF Pro Display Semibold", size: 20))
+                                
+                                Image(systemName: "play.fill")
+                                    .font(.system(size: 20, weight: .semibold))
+                            }
+                        }
+                    )
+                    .frame(width: 278, height: 48, alignment: .center)
+                    .background(Color.brandPrimary400)
+                    .foregroundColor(Color.primitiveWhite)
+                    .cornerRadius(8)
                     
                     Spacer()
                     
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("<3")
+                        Image(systemName: "square.and.arrow.up")
+                            .font(.system(size: 32, weight: .medium))
+                            .foregroundColor(Color.brandPrimary400)
                     })
                     
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("SS")
+                        Image(systemName: "heart")
+                            .font(.system(size: 32, weight: .medium))
+                            .foregroundColor(Color.brandPrimary400)
                     })
-                }
+                }.frame(maxWidth: geometry.size.width)
             }
         }
         
@@ -132,52 +212,84 @@ struct RightView: View {
     
     
     var body: some View {
-        VStack {
-            //MARK: - Ingredients
-            VStack{
-                
-                HStack{
-                    Text("Ingredientes")
-                    
-                    Spacer()
-                    
-                    Stepper(onIncrement: {
-                        incrementStep()
-                    }, onDecrement: {
-                        decrementStep()
-                    }, label: {
-                        Text("Porções: \(value)")
-                    })
-                }
-            
-                
-                ForEach(recipe.ingredients, id:\.id) { ingredient in
-                    HStack {
+        
+        ScrollView {
+            VStack {
+                //MARK: - Ingredients
+                VStack{
+                    HStack{
+                        Text("Ingredientes")
+                            .font(.custom("Albra Semi", size: 32))
+                            .foregroundColor(Color.primitiveBlack)
+                            .minimumScaleFactor(0.5)
                         
-                        Text(String(ingredient.amount * Double(value)))
-                        
-                        Text(ingredient.name)
                         
                         Spacer()
+                        
+                        Stepper(onIncrement: {
+                            incrementStep()
+                        }, onDecrement: {
+                            decrementStep()
+                        }, label: {
+                            Text("Porções: \(value)")
+                        })
+                    }
+                    
+                    ForEach(recipe.ingredients, id:\.id) { ingredient in
+                        HStack {
+                            
+                            Text(String(ingredient.amount * Double(value)))
+                                .font(.custom("SF Pro Display Regular", size: 20))
+                                .foregroundColor(Color.primitiveBlack)
+                            
+                            Text(ingredient.name)
+                                .font(.custom("SF Pro Display Regular", size: 20))
+                                .foregroundColor(Color.primitiveBlack)
+                            
+                            Spacer()
+                        }
+                    }
+                }.padding(.bottom, 80)
+                
+                Spacer()
+                
+                //MARK: - Steps
+                VStack{
+                    HStack {
+                        Text("Passo-a-Passo")
+                            .font(.custom("Albra Semi", size: 32))
+                            .foregroundColor(Color.primitiveBlack)
+                            .minimumScaleFactor(0.5)
+                        
+                        Spacer()
+                        
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Text("Começar agora")
+                            
+                        })
+                    }
+                    
+                    ForEach(recipe.stepByStep, id: \.id) { step in
+                        HStack() {
+                            Text("\(step.id)")
+                                .font(.custom("Albra Semi", size: 32))
+                                .foregroundColor(Color.brandSecondary400)
+                            
+                            Spacer()
+                            
+                            Text(step.stepByStepDescription)
+                                .font(.custom("SF Pro Display Regular", size: 20))
+                                .foregroundColor(Color.primitiveBlack)
+                            
+                            
+                        }.padding(.all, 16)
+                        
+                        Divider()
+                        
                     }
                 }
             }
-            
-            Spacer()
-            
-            //MARK: - Steps
-            VStack{
-                HStack {
-                    Text("Passo-a-Passo")
-                    
-                    Spacer()
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Começar agora")
-                        
-                    })
-                }
-            }
         }
+        
     }
 }
