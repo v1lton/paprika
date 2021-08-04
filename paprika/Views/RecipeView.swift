aefbf01fb99a68b985422387be9baf97b8c62664
Wilton Ramos <wrs@cin.ufpe.br>
2021-08-04 16:59:30 -0300

    Adjusts recipe portions font

--- paprika/Views/RecipeView.swift
+++ paprika/Views/RecipeView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 import SDWebImageSwiftUI
 
+// TODO: Mudar a cor do fundo
+// TODO: Arrumar a imagem
+// TODO: Ajustar o espaçamento entre os elementos
+// TODO: Ajustar o stepper
+
 struct RecipeView: View {
     var recipe: RecipeElement
     
@@ -26,7 +31,7 @@ struct RecipeView: View {
                 
                 Spacer()
                 
-                RightView(recipe: recipe)
+                RightView(recipe: recipe, recipePortion: recipe.portion)
                     .frame(width: geometry.size.width * 0.45)
                     .padding(.trailing, 20)
             }//.background(Color.primitive50).edgesIgnoringSafeArea(.all)
@@ -210,9 +215,9 @@ struct RightView: View {
     @State private var value: Int
     var recipe: RecipeElement
     
-    init(recipe: RecipeElement) {
+    init(recipe: RecipeElement, recipePortion: Int) {
         self.recipe = recipe
-        self.value = recipe.portion
+        self.value = recipePortion
     }
     
     func incrementStep() {
@@ -248,6 +253,29 @@ struct RightView: View {
         
         return steps
     }
+    
+    func fractionToString (fraction: Double) -> String {
+             switch fraction {
+             case 0.125..<0.126:
+                 return NSLocalizedString("\u{215B}", comment: "1/8")
+             case 0.25..<0.26:
+                 return NSLocalizedString("\u{00BC}", comment: "1/4")
+             case 0.33..<0.34:
+                 return NSLocalizedString("\u{2153}", comment: "1/3")
+             case 0.5..<0.6:
+                 return NSLocalizedString("\u{00BD}", comment: "1/2")
+             case 0.66..<0.67:
+                 return NSLocalizedString("\u{2154}", comment: "2/3")
+             case 0.75..<0.76:
+                 return NSLocalizedString("\u{00BE}", comment: "3/4")
+             default:
+                let formatter = NumberFormatter()
+                formatter.minimumFractionDigits = 0
+                formatter.maximumFractionDigits = 2
+                formatter.numberStyle = .decimal
+                return formatter.string(from: fraction as NSNumber)!
+            }
+         }
     
     
     var body: some View {
@@ -285,7 +313,7 @@ struct RightView: View {
                                 .font(.custom("SF Pro Display Regular", size: 20))
                                 .foregroundColor(Color.primitiveBlack)
                             
-                            Text(String(ingredient.amount * Double(value)))
+                            Text(fractionToString(fraction: ingredient.amount * Double(Double(value) / Double(recipe.portion))))
                                 .font(.custom("SF Pro Display Regular", size: 20))
                                 .foregroundColor(Color.primitiveBlack)
                             
@@ -295,10 +323,10 @@ struct RightView: View {
                             
                             Spacer()
                             
-                            Text(ingredient.measure)
+                            Text(ingredient.measure.lowercased())
                                 .font(.custom("SF Pro Display Regular", size: 20))
                                 .foregroundColor(Color.primitiveBlack)
-                                .lowerCase
+                            
                         }.padding(.bottom, 8)
                         .onDrag {
                             NSItemProvider(object: "\(ingredient.amount * Double(value)) \(ingredient.name)" as NSString)
