a9c670cdb7398ab67a4c1c32eb0ef6528341053a
Wilton Ramos <wrs@cin.ufpe.br>
2021-08-05 14:08:14 -0300

    Finish the UI without timer

--- paprika/Views/RecipeStepsView.swift
+++ paprika/Views/RecipeStepsView.swift
@@ -9,73 +9,6 @@ import SwiftUI
 import StepperView
 import SDWebImageSwiftUI
 
-struct FinalStepView: View {
-    @Binding var favorited: Bool
-    var recipeImage: String
-    
-    var body: some View {
-        
-        GeometryReader { geometry in
-            
-            
-            HStack(alignment: .top) {
-                Spacer()
-                
-                WebImage(url: URL(string: recipeImage))
-                    .resizable()
-                    .placeholder(Image("placeholder"))
-                    .transition(.fade(duration: 0.5))
-                    .frame(width: geometry.size.width * 0.45, height: geometry.size.height * 0.45)
-                    .scaledToFit()
-                
-                Spacer().frame(width: 16)
-                
-                VStack {
-                    Text("Bom apetite!")
-                        .font(.custom("Albra Semi", size: 56))
-                        .foregroundColor(Color.brandSecondary400)
-                        .frame(width: geometry.size.width * 0.50, alignment: .leading)
-                    
-                    Spacer().frame(height: 8)
-                    
-                    Text("Lembrou de alguém que também iria gostar dessa receita? Compartilhe! Você também pode salvá-la nos seus Favoritos para ver sempre que quiser.")
-                        .font(.custom("SF Pro Display Regular", size: 28))
-                        .lineSpacing(10)
-                    
-                    Spacer()
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Compartilhar essa receita")
-                            .font(.custom("SF Pro Display Bold", size: 24))
-                        
-                    })
-                    .frame(width: geometry.size.width * 0.50, height: 56, alignment: .center)
-                    .background(Color.primitiveWhite)
-                    .foregroundColor(Color.brandPrimary400)
-                    .cornerRadius(8)
-                    
-                    Spacer().frame(maxHeight: 16)
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Salvar nos favoritos")
-                            
-                            .font(.custom("SF Pro Display Bold", size: 24))
-                    })
-                    .frame(width: geometry.size.width * 0.50, height: 56, alignment: .center)
-                    .background(Color.primitiveWhite)
-                    .foregroundColor(Color.brandPrimary400)
-                    .cornerRadius(8)
-                    
-                }.frame(width: geometry.size.width * 0.50, height: geometry.size.height * 0.45)
-                
-                Spacer()
-                
-            }
-            
-        }
-    }
-}
-
 struct RecipeStepsView: View {
     
     @StateObject var viewModel: ViewModel
@@ -105,10 +38,9 @@ struct RecipeStepsView: View {
                                 .frame(width: geometry.size.width * 0.3, alignment: .leading)
                                 .font(.custom("Albra Semi", size: 56))
                                 .foregroundColor(Color.brandSecondary400)
-                            
+                                .minimumScaleFactor(0.3)
                             
                             Spacer()
-                            
                             
                             //MARK: - Description and timer box
                             VStack {
@@ -117,21 +49,32 @@ struct RecipeStepsView: View {
                                     .font(.custom("SF Pro Display Regular", size: 32))
                                     .onDrag {
                                         NSItemProvider(object: step.stepByStepDescription as NSString)
-                                     }
+                                    }
                                 
                                 Spacer().frame(height: 32)
                                 
                                 if let timer = step.timeInMinutes {
                                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                                        Text("Ativar timer (\(timer) min)")
-                                            .foregroundColor(Color.brandPrimary400)
-                                            .font(.custom("SF Pro Display Bold", size: 24))
+                                        HStack {
+                                            Text("Ativar timer (\(timer) min)")
+                                                .font(.custom("SF Pro Display Bold", size: 24))
+                                                .lineLimit(1)
+                                                .minimumScaleFactor(0.3)
+                                            
+                                            Image(systemName: "clock")
+                                                .font(.custom("SF Pro Display Bold", size: 24))
+                                                .minimumScaleFactor(0.3)
+                                        }
+                                        
                                     })
                                     .frame(width: geometry.size.width * 0.60, height: 56, alignment: .center)
-                                    .foregroundColor(Color.primitiveWhite)
                                     .background(Color.primitiveWhite)
                                     .foregroundColor(Color.brandPrimary400)
                                     .cornerRadius(8)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 8)
+                                            .stroke(Color.primitive200, lineWidth: 1)
+                                    )
                                 }
                                 
                                 
@@ -144,7 +87,7 @@ struct RecipeStepsView: View {
                     
                     
                 }
-                FinalStepView(favorited: Binding.constant(false), recipeImage: self.recipeImage)
+                FinalStepView(recipeImage: self.recipeImage)
                 
             }.tabViewStyle(PageTabViewStyle())
             .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
@@ -154,29 +97,100 @@ struct RecipeStepsView: View {
     }
 }
 
-struct FinalStepView_Previes: PreviewProvider {
-    static var previews: some View {
-        Group {
-            FinalStepView(favorited: Binding.constant(false), recipeImage: "https://images.unsplash.com/photo-1568240464340-261c0f65a455?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80")
-            FinalStepView(favorited: Binding.constant(false), recipeImage: "https://images.unsplash.com/photo-1568240464340-261c0f65a455?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80")
-                .previewDevice("iPad Pro (12.9-inch) (5th generation)")
+struct FinalStepView: View {
+    @State var favorited: Bool = false
+    var recipeImage: String
+    
+    var body: some View {
+        
+        GeometryReader { geometry in
+            
+            
+            HStack(alignment: .top) {
+                Spacer()
+                
+                WebImage(url: URL(string: recipeImage))
+                    .resizable()
+                    .placeholder(Image("placeholder"))
+                    .transition(.fade(duration: 0.5))
+                    .scaledToFill()
+                    .frame(width: geometry.size.width * 0.45, height: geometry.size.height * 0.45)
+                    .clipped()
+                
+                Spacer().frame(width: 16)
+                
+                VStack {
+                    Text("Bom apetite!")
+                        .font(.custom("Albra Semi", size: 56))
+                        .foregroundColor(Color.brandSecondary400)
+                        .frame(width: geometry.size.width * 0.50, alignment: .leading)
+                    
+                    Spacer().frame(height: 8)
+                    
+                    Text("Lembrou de alguém que também iria gostar dessa receita? Compartilhe! Você também pode salvá-la nos seus Favoritos para ver sempre que quiser.")
+                        .font(.custom("SF Pro Display Regular", size: 28))
+                        .lineSpacing(10)
+                        .minimumScaleFactor(0.3)
+                    
+                    Spacer()
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        HStack {
+                            
+                            Text("Compartilhar essa receita")
+                                .font(.custom("SF Pro Display Bold", size: 24))
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.3)
+                            
+                            Image(systemName: "square.and.arrow.up")
+                                .font(.custom("SF Pro Display Bold", size: 24))
+                                .minimumScaleFactor(0.3)
+                        }
+                    })
+                    .frame(width: geometry.size.width * 0.50, height: 56, alignment: .center)
+                    .background(Color.primitiveWhite)
+                    .foregroundColor(Color.brandPrimary400)
+                    .cornerRadius(8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.primitive200, lineWidth: 1)
+                    )
+                    
+                    Spacer().frame(maxHeight: 16)
+                    
+                    Button(action: {
+                        favorited.toggle()
+                    }, label: {
+                        HStack {
+                            Text(favorited ? "Receita Salva!" : "Salvar Receita" )
+                                .font(.custom("SF Pro Display Bold", size: 24))
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.3)
+                            
+                            Image(systemName: favorited ? "heart.fill" : "heart")
+                                .font(.custom("SF Pro Display Bold", size: 24))
+                                .minimumScaleFactor(0.3)
+                        }
+                        
+                    })
+                    .frame(width: geometry.size.width * 0.50, height: 56, alignment: .center)
+                    .background(favorited ? Color.brandPrimary400 : Color.primitiveWhite)
+                    .foregroundColor(favorited ? Color.primitiveWhite : Color.brandPrimary400)
+                    .cornerRadius(8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(favorited ? Color.brandPrimary400 : Color.primitive200, lineWidth: 1)
+                    )
+                    
+                }.frame(width: geometry.size.width * 0.50, height: geometry.size.height * 0.45)
+                
+                Spacer()
+                
+            }
+            
         }
     }
 }
-
-//struct RecipeStepsView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        
-//        let viewModel = RecipeStepsView.ViewModel()
-//        
-//        Group {
-//            RecipeStepsView(viewModel: viewModel)
-//            RecipeStepsView(viewModel: viewModel)
-//                .previewDevice("iPad Pro (12.9-inch) (5th generation)")
-//        }
-//        
-//    }
-//}
 
 //TODO: Fazer um adapative stack baseado no geometryReader
 
